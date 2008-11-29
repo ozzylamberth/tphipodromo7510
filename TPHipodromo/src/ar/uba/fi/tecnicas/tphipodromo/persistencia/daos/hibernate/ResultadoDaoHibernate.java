38503bd29cc85357609256c63187fac1461be9c4
nahuelcampo <nahuelcampo@64bfb3c8-9865-11dd-a26a-893737b22a74>
2008-11-29 01:21:18 +0000

    implementacion del ResultadoDaoHibernate

---
+++ TPHipodromo/src/ar/uba/fi/tecnicas/tphipodromo/persistencia/daos/hibernate/ResultadoDaoHibernate.java
@@ -0,0 +1,8 @@
+package ar.uba.fi.tecnicas.tphipodromo.persistencia.daos.hibernate;
+
+import ar.uba.fi.tecnicas.tphipodromo.modelo.Resultado;
+import ar.uba.fi.tecnicas.tphipodromo.persistencia.daos.ResultadoDao;
+
+public class ResultadoDaoHibernate extends HibernateDaoGenerico<Resultado> implements ResultadoDao {
+
+}
