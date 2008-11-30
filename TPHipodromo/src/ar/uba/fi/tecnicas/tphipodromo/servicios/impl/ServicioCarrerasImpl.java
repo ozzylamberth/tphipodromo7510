8aceb93503cea2ee98fe9d1864505b179c51b97d
nahuelcampo <nahuelcampo@64bfb3c8-9865-11dd-a26a-893737b22a74>
2008-11-30 19:08:57 +0000

    - servicio para que se busquen los caballos y los jockeys que no estan en una carrera - correccion en el nombre de un transformer

--- TPHipodromo/src/ar/uba/fi/tecnicas/tphipodromo/servicios/impl/ServicioCarrerasImpl.java
+++ TPHipodromo/src/ar/uba/fi/tecnicas/tphipodromo/servicios/impl/ServicioCarrerasImpl.java
@@ -8,32 +8,44 @@ import java.util.Iterator;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.Transformer;
 
+import ar.uba.fi.tecnicas.tphipodromo.modelo.Caballo;
 import ar.uba.fi.tecnicas.tphipodromo.modelo.Carrera;
 import ar.uba.fi.tecnicas.tphipodromo.modelo.EstadoCarrera;
+import ar.uba.fi.tecnicas.tphipodromo.modelo.Jockey;
 import ar.uba.fi.tecnicas.tphipodromo.modelo.Participante;
 import ar.uba.fi.tecnicas.tphipodromo.modelo.excepciones.HipodromoException;
+import ar.uba.fi.tecnicas.tphipodromo.persistencia.daos.CaballoDao;
 import ar.uba.fi.tecnicas.tphipodromo.persistencia.daos.CarreraDao;
 import ar.uba.fi.tecnicas.tphipodromo.persistencia.daos.DAOGenerico;
+import ar.uba.fi.tecnicas.tphipodromo.persistencia.daos.JockeyDao;
 import ar.uba.fi.tecnicas.tphipodromo.persistencia.daos.excepciones.ObjetoInexistenteException;
 import ar.uba.fi.tecnicas.tphipodromo.persistencia.daos.hibernate.HibernateDaoFactory;
 import ar.uba.fi.tecnicas.tphipodromo.servicios.ServicioCarreras;
+import ar.uba.fi.tecnicas.tphipodromo.servicios.dtos.CaballoDTO;
 import ar.uba.fi.tecnicas.tphipodromo.servicios.dtos.CarreraDTO;
+import ar.uba.fi.tecnicas.tphipodromo.servicios.dtos.JockeyDTO;
 import ar.uba.fi.tecnicas.tphipodromo.servicios.dtos.ParticipanteDTO;
 import ar.uba.fi.tecnicas.tphipodromo.servicios.excepciones.EntidadInexistenteException;
 import ar.uba.fi.tecnicas.tphipodromo.servicios.excepciones.ErrorHipodromoException;
+import ar.uba.fi.tecnicas.tphipodromo.servicios.transformers.CaballoTransformerToDTO;
 import ar.uba.fi.tecnicas.tphipodromo.servicios.transformers.CarreraTransformerFromDTO;
 import ar.uba.fi.tecnicas.tphipodromo.servicios.transformers.CarreraTransformerToDTO;
+import ar.uba.fi.tecnicas.tphipodromo.servicios.transformers.JockeyTransformerToDTO;
 import ar.uba.fi.tecnicas.tphipodromo.servicios.transformers.ParticipanteTransformerFromDTO;
 
 @SuppressWarnings("serial")
 public class ServicioCarrerasImpl extends ServicioIdentificableImpl<Carrera, CarreraDTO> implements ServicioCarreras {
 	
 	private CarreraDao carreraDao;
+	private CaballoDao caballoDao;
+	private JockeyDao jockeyDao;
 	private CarreraTransformerFromDTO carreraTransformerFromDTO = new CarreraTransformerFromDTO();
 	private CarreraTransformerToDTO carreraTransformerToDTO = new CarreraTransformerToDTO();
 	
 	public ServicioCarrerasImpl() {
 		this.carreraDao = HibernateDaoFactory.getInstance().getCarreraDAO();
+		this.caballoDao = HibernateDaoFactory.getInstance().getCaballoDAO();
+		this.jockeyDao = HibernateDaoFactory.getInstance().getJockeyDAO();
 	}
 
 	public DAOGenerico<Carrera> getDao() {
@@ -123,6 +135,58 @@ public class ServicioCarrerasImpl extends ServicioIdentificableImpl<Carrera, Car
 			result.add(estadosSiguientes[i].getNombre());
 		}
 		return result;
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public Collection<CaballoDTO> buscarCaballosFueraDeCarrera(CarreraDTO carreraDTO) {
+		Carrera carrera = (Carrera) this.getTransformerFromDTO().transform(carreraDTO);
+		Collection<Caballo> caballosFueraDeCarrera = new ArrayList<Caballo>();
+		Iterator<Caballo> it = this.caballoDao.buscarTodos().iterator();
+		while (it.hasNext()) {
+			Caballo caballo = (Caballo) it.next();
+			if (!this.caballoEstaEnCarrera(carrera, caballo)) {
+				caballosFueraDeCarrera.add(caballo);
+			}
+		}
+		return CollectionUtils.collect(caballosFueraDeCarrera, new CaballoTransformerToDTO());
+	}
+	
+	private boolean caballoEstaEnCarrera(Carrera carrera, Caballo caballo) {
+		Iterator<Participante> it = carrera.getParticipantes().iterator();
+		while (it.hasNext()) {
+			Participante participante = (Participante) it.next();
+			if (participante.getCaballo().equals(caballo)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public Collection<JockeyDTO> buscarJockeysFueraDeCarrera(CarreraDTO carreraDTO) {
+		Carrera carrera = (Carrera) this.getTransformerFromDTO().transform(carreraDTO);
+		Collection<Jockey> jockeysFueraDeCarrera = new ArrayList<Jockey>();
+		Iterator<Jockey> it = this.jockeyDao.buscarTodos().iterator();
+		while (it.hasNext()) {
+			Jockey jockey = (Jockey) it.next();
+			if (!this.jockeyFueraDeCarrera(carrera, jockey)) {
+				jockeysFueraDeCarrera.add(jockey);
+			}
+		}
+		return CollectionUtils.collect(jockeysFueraDeCarrera, new JockeyTransformerToDTO());
+	}
+	
+	private boolean jockeyFueraDeCarrera(Carrera carrera, Jockey jockey) {
+		Iterator<Participante> it = carrera.getParticipantes().iterator();
+		while (it.hasNext()) {
+			Participante participante = (Participante) it.next();
+			if (participante.getJockey().equals(jockey)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
