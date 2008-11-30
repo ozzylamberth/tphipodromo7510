97eacbcc276d6e95210e763555ffe234ec7b52e6
juan.grande <juan.grande@64bfb3c8-9865-11dd-a26a-893737b22a74>
2008-11-30 17:05:12 +0000

    Agregué la inicialización de Hibernate a los tests de persistencia.

---
+++ trunk/TPHipodromo/test/ar/uba/fi/tecnicas/tphipodromo/persistencia/dao/test/PersistenciaTestSuite.java
@@ -0,0 +1,19 @@
+package ar.uba.fi.tecnicas.tphipodromo.persistencia.dao.test;
+
+import junit.framework.TestSuite;
+
+public class PersistenciaTestSuite extends TestSuite {
+
+	public static TestSuite suite() {
+		TestSuite suite = new TestSuite();
+		
+		suite.addTestSuite(BolsaApuestasDaoTest.class);
+		suite.addTestSuite(CaballoDaoTest.class);
+		suite.addTestSuite(CarreraDaoTest.class);
+		suite.addTestSuite(JockeyDaoTest.class);
+		suite.addTestSuite(ParticipanteDaoTest.class);
+		
+		return suite;
+	}
+	
+}
