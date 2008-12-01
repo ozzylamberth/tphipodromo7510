cb237826db50cc7cf312133d79d56ddfa8142895
juan.grande <juan.grande@64bfb3c8-9865-11dd-a26a-893737b22a74>
2008-12-01 05:05:49 +0000

    Agregué la descripción en algunas excepciones que no la tenían.

--- TPHipodromo/src/ar/uba/fi/tecnicas/tphipodromo/servicios/excepciones/ApuestaInvalidaException.java
+++ TPHipodromo/src/ar/uba/fi/tecnicas/tphipodromo/servicios/excepciones/ApuestaInvalidaException.java
@@ -5,6 +5,10 @@ import com.google.gwt.user.client.rpc.IsSerializable;
 @SuppressWarnings("serial")
 public class ApuestaInvalidaException extends Exception implements IsSerializable {
 	
+	public ApuestaInvalidaException(String message) {
+		super(message);
+	}
+	
 	public ApuestaInvalidaException() {
 		super("La apuesta tiene datos invalidos");
 	}
