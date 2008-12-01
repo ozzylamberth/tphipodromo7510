1d3a899f87788c50ccf420cc8af7aa3d20faefe6
a.irrera <a.irrera@64bfb3c8-9865-11dd-a26a-893737b22a74>
2008-12-01 03:15:15 +0000

    Se agrega imagen a la pagina.

--- TPHipodromo/src/ar/uba/fi/tecnicas/tphipodromo/client/vista/impl/PanelTop.java
+++ TPHipodromo/src/ar/uba/fi/tecnicas/tphipodromo/client/vista/impl/PanelTop.java
@@ -2,6 +2,7 @@ package ar.uba.fi.tecnicas.tphipodromo.client.vista.impl;
 
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Hyperlink;
+import com.google.gwt.user.client.ui.Image;
 
 public class PanelTop extends HorizontalPanel {
 
@@ -13,8 +14,10 @@ public class PanelTop extends HorizontalPanel {
 	private void init() {
 		HorizontalPanel auxPanel1 = new HorizontalPanel();
 		HorizontalPanel auxPanel2 = new HorizontalPanel();
+		Image header = new Image("images/header.png");
+		auxPanel1.add(header);
 		this.setSpacing(1);
-		auxPanel1.setSize("100%", "5%");
+		auxPanel1.setSize("100%", "30%");
 		auxPanel2.setSpacing(5);
 		this.setTitle("panelTop");
 		this.setSize("100%", "5%");
@@ -22,10 +25,11 @@ public class PanelTop extends HorizontalPanel {
 		this.add(auxPanel1);
 		this.add(auxPanel2);
 		
-		Hyperlink home = new Hyperlink("home", false, "home");
+		/*Hyperlink home = new Hyperlink("home", false, "home");
 		auxPanel2.add(home);//TODO implementar history
 		
 		Hyperlink logOut = new Hyperlink("log Out", false, "logOut");
 		auxPanel2.add(logOut);//TODO deslogear
+		*/
 	}
 }
