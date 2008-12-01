8c3110c80591ad2218d84ca4aa7412314fddce5d
juan.grande <juan.grande@64bfb3c8-9865-11dd-a26a-893737b22a74>
2008-12-01 02:25:31 +0000

    Corregí que solo liste las carreras apostables.

--- trunk/TPHipodromo/src/ar/uba/fi/tecnicas/tphipodromo/persistencia/daos/hibernate/CarreraDaoHibernate.java
+++ trunk/TPHipodromo/src/ar/uba/fi/tecnicas/tphipodromo/persistencia/daos/hibernate/CarreraDaoHibernate.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.hibernate.Query;
 
 import ar.uba.fi.tecnicas.tphipodromo.modelo.Carrera;
+import ar.uba.fi.tecnicas.tphipodromo.modelo.EstadoCarrera;
 import ar.uba.fi.tecnicas.tphipodromo.persistencia.daos.CarreraDao;
 import ar.uba.fi.tecnicas.tphipodromo.persistencia.daos.excepciones.MultiplesObjetosException;
 import ar.uba.fi.tecnicas.tphipodromo.persistencia.daos.excepciones.ObjetoInexistenteException;
@@ -57,7 +58,23 @@ public class CarreraDaoHibernate extends HibernateDaoGenerico<Carrera> implement
 
 	@Override
 	public Collection<Carrera> buscarCarrerasApostablesPorFecha(Date fecha) {
-		// TODO implementar logica
-		return null;
+		Date fecha1 = new Date();
+		Calendar cal = Calendar.getInstance(); 
+		
+		cal.setTime(fecha);
+		cal.set(Calendar.HOUR,0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND,0);
+
+		fecha = cal.getTime();
+		cal.add(Calendar.DATE, 1);
+		fecha1 = cal.getTime();
+		
+		Query q = getSession().createQuery("from Carrera c where c.fechaYHora > :p and c.fechaYHora< :q and c.estadoCarrera = :e");
+		q.setDate("p",  fecha);
+		q.setDate("q",  fecha1);
+		q.setString("e", EstadoCarrera.ABIERTA_A_APUESTAS.name());
+		Collection<Carrera> listaCarreras = q.list();
+		return listaCarreras;
 	}
 }
